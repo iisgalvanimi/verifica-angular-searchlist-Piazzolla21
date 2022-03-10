66b3aa0b61fc508b2a8eeddbff28c5d7ed6c82f3
Piazzolla21 <piazzolla.mathias@iisgalvanimi.edu.it>
2022-03-10 08:51:26 +0000

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Empty-Project';
+  comment:string=""
+  spesa : {dati : string, visibile : boolean}[] = [{dati :"banana",visibile :false},
+  {dati:"fragola",visibile :false},{dati : "lampone", visibile :false},{dati : "melone",visibile :false}];
+  search(){
+    console.log("ciao")
+    for (let i = 0; i < this.spesa.length ; i++){
+      console.log(this.spesa[i].dati.search(this.comment))
+      if (this.spesa[i].dati.search(this.comment) == -1){
+        this.spesa[i].visibile = true;
+      }
+      else{ 
+        this.spesa[i].visibile = false;
+      }
+    }
+
+  }
 }
